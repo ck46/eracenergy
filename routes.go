531b3c12e256ca2b5f878936a0dfda17af9cee95
Sh1r0 <prof.chakas@gmail.com>
2020-08-27 05:57:04 +0900

    Added new pages for solutions

--- routes.go
+++ routes.go
@@ -29,5 +29,13 @@ func router() *mux.Router {
 	r.HandleFunc("/blog", web.BlogHandler).Methods("GET")
 	r.HandleFunc("/contact", web.ContactHandler).Methods("GET", "POST")
 
+	// solutions
+	r.HandleFunc("/off-grid-solar-pv-solutions", web.OffGridHandler).Methods("GET")
+	r.HandleFunc("/solar-hybrid-and-battery-backup", web.SolarHybridHandler).Methods("GET")
+	r.HandleFunc("/solar-street-lights", web.SolarLightsHandler).Methods("GET")
+	r.HandleFunc("/solar-pumps-and-geysers", web.SolarPumpsHandler).Methods("GET")
+	r.HandleFunc("/solar-energy-kits-and-home-systems", web.SolarEnergyHandler).Methods("GET")
+	r.HandleFunc("/technical-consultancy", web.TechnicalHandler).Methods("GET")
+
 	return r
 }
--- web/static_handlers.go
+++ web/static_handlers.go
@@ -36,3 +36,39 @@ func SolutionsHandler(w http.ResponseWriter, r *http.Request) {
 	tpl := "templates/solutions.tmpl"
 	GenericHandler(w, r, tpl, title)
 }
+
+func OffGridHandler(w http.ResponseWriter, r *http.Request) {
+	title := "ERAC Energy - OFF-GRID SOLAR PV SOLUTIONS"
+	tpl := "templates/off-grid-solar.tmpl"
+	GenericHandler(w, r, tpl, title)
+}
+
+func SolarHybridHandler(w http.ResponseWriter, r *http.Request) {
+	title := "ERAC Energy - SOLAR HYBRID AND BATTERY BACKUP SOLUTIONS"
+	tpl := "templates/solar-hybrid.tmpl"
+	GenericHandler(w, r, tpl, title)
+}
+
+func SolarLightsHandler(w http.ResponseWriter, r *http.Request) {
+	title := "ERAC Energy - SOLAR STREET LIGHTS"
+	tpl := "templates/solar-street-lights.tmpl"
+	GenericHandler(w, r, tpl, title)
+}
+
+func SolarPumpsHandler(w http.ResponseWriter, r *http.Request) {
+	title := "ERAC Energy - SOLAR PUMPS AND GEYSERS"
+	tpl := "templates/solar-pumps.tmpl"
+	GenericHandler(w, r, tpl, title)
+}
+
+func SolarEnergyHandler(w http.ResponseWriter, r *http.Request) {
+	title := "ERAC Energy - SOLAR ENERGY KITS AND SOLAR HOME SYSTEMS"
+	tpl := "templates/solar-energy-kits.tmpl"
+	GenericHandler(w, r, tpl, title)
+}
+
+func TechnicalHandler(w http.ResponseWriter, r *http.Request) {
+	title := "ERAC Energy - TECHNICAL & CONSULTANCY SERVICES"
+	tpl := "templates/technical-consultancy.tmpl"
+	GenericHandler(w, r, tpl, title)
+}
